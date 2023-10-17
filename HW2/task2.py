7aa25747db81560a290ec46be85e5481ff085914
Erza Berbatovci <116487812+erza-b@users.noreply.github.com>
2023-10-17 10:53:43 +0200

    added-homework2

---
+++ HW2/task2.py
@@ -0,0 +1,12 @@
+#Task 2
+
+#Manipulate strings.
+
+#Save your first and last name as separate variables, then use string concatenation to add them together with a white space in between and print a greeting.
+
+firstName = "Erza"
+lastName = "Bërbatovci"
+
+myFullName = firstName + " " + lastName
+
+print(myFullName)
---
+++ HW2/task3.py
@@ -0,0 +1,32 @@
+#Using python as a calculator.
+
+#Make a program with 2 numbers saved in separate variables a and b, then print the result for each of the following: 
+
+#Addition
+#Subtraction
+#Division
+#Multiplication
+#Exponent (Power)
+#Modulus
+#Floor division
+
+a = 2
+b = 3
+
+addition = a + b
+subtraction = a - b
+division = a / b
+multiplication = a * b
+exponent = a ** b
+modulus = a % b
+floor_division = a // b
+
+print(f"Addition of 2+3 = {addition}")
+print(f"Subtraction of 2-3 = {subtraction}")
+print(f"Division of 2/3 = {division}")
+print(f"Multiplication of 2*3 = {multiplication}")
+print(f"Exponent of 2^3 = {exponent}")
+print(f"Modulus of 2%3 = {modulus}")
+print(f"Floor Division of 2//3 = {floor_division}")
+
+
