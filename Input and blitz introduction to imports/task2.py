3efcd4f31f09374e358cf207cef39209d8b010aa
Erza Berbatovci <116487812+erza-b@users.noreply.github.com>
2023-10-18 16:55:26 +0200

    task2

---
+++ Input and blitz introduction to imports/task2.py
@@ -0,0 +1,10 @@
+#The birthday greeting program.
+
+#Write a program that takes your name as input, and then your age as input and greets you with the following:
+
+#"Hello <name>, on your next birthday you’ll be <age+1> years"
+
+name="Erza"
+age=20
+
+print(f"Hello {name}, on your next birthdat you'll be {age+1}")
