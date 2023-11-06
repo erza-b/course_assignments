03ddabe566cfda50364965249f2a93c65cee5b65
Erza Berbatovci <116487812+erza-b@users.noreply.github.com>
2023-11-06 10:13:32 +0100

    added-task1

---
+++ Functions as first-class objects/task1.py
@@ -0,0 +1,16 @@
+#Task 1
+
+#Write a Python program to detect the number of local variables declared in a function.
+
+
+def count_local_variables():
+    a = 1
+    b = 'hello'
+    c = [1, 2, 3]
+    d = {'name': 'John', 'age': 30}
+    
+     
+    return locals()
+
+num_locals = count_local_variables()
+print(f"Number of local variables in the function: {num_locals}")
